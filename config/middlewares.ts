ae72526bc5208caf0a893c1b23606aac2e67397d
paul <paul@Mac.localdomain>
2025-06-20 18:47:20 -0500

    update seed file

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,27 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net', 'api.mapbox.com'],
+          'img-src': ["'self'", 'data:', 'blob:' ],
+          'media-src': ["'self'", 'data:', 'blob:'],
+          'worker-src': ['blob:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default () => ({
+  // "strapi-plugin-map-box": {
+  //   enabled: true,
+  //   resolve: "/Users/paul/work/plugin-dev/plugins/strapi-plugin-map-box",
+  //   config: {
+  //     public: {
+  //       accessToken:
+  //         "pk.eyJ1IjoiY29kaW5nYWZ0ZXJ0aGlydHkiLCJhIjoiY21idjY0YzB0MGw2NzJpcHZienVoa2xldyJ9.payvQGiBTMzodS2cjTqW0Q",
+  //       debugMode: false,
+  //     },
+  //   },
+  // },
+});
